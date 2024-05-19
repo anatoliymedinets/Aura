8e022c56ce29b9b5b3dbdfdc29dc59e679c34280
Anatoliy Medinets <a.medinets@gmail.com>
2024-05-19 19:07:19 +0300

    11. Game Mode

--- Source/Aura/Private/Characters/AuraCharacter.cpp
+++ Source/Aura/Private/Characters/AuraCharacter.cpp
@@ -2,4 +2,44 @@
 
 
 #include "Characters/AuraCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Camera/CameraComponent.h"
 
+AAuraCharacter::AAuraCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SetupCamera();
+	SetupMovement();
+}
+
+void AAuraCharacter::SetupCamera()
+{
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(GetRootComponent());
+	SpringArm->AddWorldRotation(FRotator(-45.f, 0.f, 0.f).Quaternion());
+	SpringArm->TargetArmLength = 750.f;
+	SpringArm->bUsePawnControlRotation = false;
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritRoll = false;
+	SpringArm->bInheritYaw = false;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+}
+
+void AAuraCharacter::SetupMovement()
+{
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f); // скорость поворота
+	GetCharacterMovement()->bConstrainToPlane = true;
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	// Отключить вращение контроллера по всем осям
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
