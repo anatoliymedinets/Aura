55835d41ff04c77482ec2ad484f771c03f0aeb49
Anatoliy Medinets <a.medinets@gmail.com>
2024-05-20 16:40:33 +0300

    12. Enemy Interface

---
+++ Source/Aura/Private/Interaction/EnemyInterface.cpp
@@ -0,0 +1,6 @@
+// Copyright AM Company
+
+
+#include "Interaction/EnemyInterface.h"
+
+// Add default functionality here for any IEnemyInterface functions that are not pure virtual.
--- Source/Aura/Public/Characters/AuraEnemy.h
+++ Source/Aura/Public/Characters/AuraEnemy.h
@@ -4,14 +4,24 @@
 
 #include "CoreMinimal.h"
 #include "Characters/AuraCharacterBase.h"
+#include "Interaction/EnemyInterface.h"
 #include "AuraEnemy.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AURA_API AAuraEnemy : public AAuraCharacterBase
+class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
 	
+public:
+	// Inherited via IEnemyInterface
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
+
+protected:
+
+private:
+
 };
