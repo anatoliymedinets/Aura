67170deb74363a582291fdf8440dec2196619504
Anatoliy Medinets <a.medinets@gmail.com>
2024-05-23 17:58:12 +0300

    28. Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/WidgetControllers/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright AM Company
+
+
+#include "UI/WidgetControllers/AuraWidgetController.h"
+
---
+++ Source/Aura/Private/UI/Widgets/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright AM Company
+
+
+#include "UI/Widgets/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
