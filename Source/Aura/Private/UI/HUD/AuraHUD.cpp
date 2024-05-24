b87ff64638f41d13ab60a92f60d088a58f5f5ccb
Anatoliy Medinets <a.medinets@gmail.com>
2024-05-24 13:23:35 +0300

    31. Aura HUD

---
+++ Source/Aura/Private/UI/HUD/AuraHUD.cpp
@@ -0,0 +1,14 @@
+// Copyright AM Company
+
+
+#include "UI/HUD/AuraHUD.h"
+#include "UI/Widgets/AuraUserWidget.h"
+
+void AAuraHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UUserWidget* Widget = CreateWidget<UUserWidget>(GetWorld(), OverlayWidgetClass);
+	Widget->AddToViewport();
+
+}
