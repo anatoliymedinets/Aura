b502ce469b51ad95c96f75babb456ad004dd355c
Anatoliy Medinets <a.medinets@gmail.com>
2024-06-03 18:32:48 +0300

    89. Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetControllers/AttributeMenuWidgetController.cpp
@@ -0,0 +1,12 @@
+// Copyright AM Company
+
+
+#include "UI/WidgetControllers/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}
---
+++ Source/Aura/Public/UI/WidgetControllers/AttributeMenuWidgetController.h
@@ -0,0 +1,21 @@
+// Copyright AM Company
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetControllers/AuraWidgetController.h"
+#include "AttributeMenuWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAttributeMenuWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+
+public:
+	virtual void BroadcastInitialValues() override;
+	virtual void BindCallbacksToDependencies() override;
+	
+};
