6da05e0ad9c9999c967a4ab60438d3c4ac9f6c50
Anatoliy Medinets <a.medinets@gmail.com>
2024-06-04 17:05:41 +0300

    93. Widget Attribute Tags

--- Source/Aura/Private/UI/WidgetControllers/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetControllers/AttributeMenuWidgetController.cpp
@@ -16,6 +16,7 @@ void UAttributeMenuWidgetController::BroadcastInitialValues()
 	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(FAuraGameplayTags::Get().Attributes_Primary_Strength);
 	Info.AttributeValue = AS->GetStrength();
 	AttributeInfoDelegate.Broadcast(Info);
+
 }
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
